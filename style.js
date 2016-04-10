0366659ec4082b16e49f3705aff2a5792780689c
Ankur Jain <ankurjain.v@flipkart.com>
2016-04-10 14:02:34 +0530

    Increase width of container

--- style.js
+++ style.js
@@ -4,7 +4,7 @@ module.exports = React.StyleSheet.create({
   container: {
     borderWidth: 0.5,
     borderRadius: 4,
-    width: 91,
+    width: 110,
     flexDirection: 'row',
     overflow: 'hidden'
   },
